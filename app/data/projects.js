dde626a70b73aaead1a0a8db6968d2e677a5d2b6
LucSaunders <lucsaunders@gmail.com>
2019-06-28 19:59:15 -0400

    Update projects data.

--- app/data/projects.js
+++ app/data/projects.js
@@ -5,20 +5,20 @@ const data = [
     subtitle: 'A Hubspot clone ',
     link: 'https://project-hubspot.herokuapp.com/dashboard',
     image: '/images/projects/axon.jpg',
-    date: '2015-11-20',
+    date: '2019-05-10',
     desc:
-      'Axon is an online Customer-Relationship-Management (CRM) tool for tracking company revenue, current and past clients, and deals in various stages. The frontend was designed with React, Redux, and Sass. The backend is written in Node and utilizes the Express framework and a MySQL database. This application was developed by an eleven-member team during the Project Shift Software Engineering Fellowship in May 2019.'
+      'Axon is a Customer Relationship Management tool for tracking company revenue, current and past clients, and deals in various stages. The frontend was designed with React, Redux, and Sass. The backend is written in Node and utilizes the Express framework and a MySQL database. This application was developed by an eleven-member team of Project Shift software-engineering fellows.'
   },
   {
     title: 'First Listen',
     subtitle: '2019 Project Shift Hackathon',
-    link: 'https://firstlisten.com/software/',
-    image: '/images/projects/firstlisten2.jpg',
+    link: '#',
+    image: '/images/projects/firstlisten.jpg',
     date: '2019-03-31',
     desc:
       'Designed for music lovers on the go, when they find themselves in a new town. ' +
-      'First Listen allows users to check out upcoming music events by location and preview YouTube clips of the performing artists, as an aid to making exciting evening plans in unfamiliar places. ' +
-      'First Listen was developed in a team of three Project Shift engineering fellows. '
+      'Check out upcoming music events by location and preview YouTube clips of the performing artists listed, as an aid to making exciting evening plans in unfamiliar places. ' +
+      'First Listen was developed in a team of three Project Shift software engineering fellows. '
   },
   {
     title: 'Sherpa Book Review',
@@ -30,16 +30,16 @@ const data = [
   },
   {
     title: 'SpaceX Launch Missions',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
+    subtitle: 'Tracking successful and failed SpaceX rocket launches.',
     link: 'https://spacex-missions-gql.herokuapp.com/',
     image: '/images/projects/spacex2.jpg',
-    // date: '2019-??-??',
-    desc: 'A React client using GraphQL to query the SpaceX API.'
+    date: '2019-06-25',
+    desc: 'A React client using GraphQL with Apollo to query the SpaceX API.'
   },
   {
     title: 'Lyrica',
     subtitle: 'Search for lyrics to any song',
-    link: '/project/lyrica/',
+    link: '#',
     image: '/images/projects/lyrica.jpg',
     date: '2019-06-27',
     desc: 'Search for lyrics to any song.'
@@ -49,7 +49,7 @@ const data = [
     subtitle: '',
     link: 'https://lucsaunders.github.io/contact-organizer',
     image: '/images/projects/contactorganizer.jpg',
-    date: '2019-05-',
+    date: '2019-03-25',
     desc: 'Manage personal contacts.'
   }
 ];
