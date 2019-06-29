f11cc94a12e4915d5d6455d1d1ab3e456d46c07c
LucSaunders <lucsaunders@gmail.com>
2019-06-29 16:47:08 -0400

    Add Experience component to Resume. Add positions to resume data file.

---
+++ app/data/resume/positions.js
@@ -0,0 +1,74 @@
+const positions = [
+  {
+    company: 'Project Shift',
+    position: 'Fullstack Software Engineering Fellow',
+    link: 'https://www.projectshift.io',
+    daterange: 'February 2019 - Present',
+    points: [
+      'Engaged in 800+ hours learning and developing full-stack apps in competitive full-time immersive certificate program',
+      'Developed optimized front-end apps by handling MVC data flow',
+      'Engineered back-end apps and RESTful APIs using Node with Express',
+      'Programmed 20+ web projects independently, via pair-programming, and in a team utilizing Agile methodologies',
+      'Completed projects integrating computer science principles and current industry best practices'
+    ]
+  },
+  {
+    company: 'The Sun Magazine',
+    position: 'Assistant Editor',
+    link: 'https://www.thesunmagazine.org',
+    daterange: 'January 2012 - January 2019',
+    points: [
+      'Assisted on a major website redesign, a transition from print to digital media, and indexing',
+      'Solicited work from writers, evaluated submissions, fact-checked and copyedited interview transcripts and prose manuscripts for publication in an independent, nationally-distributed monthly magazine with more than 75,000 subscribers',
+      'Acted as liaison to interviewers, interviewees, and authors to develop works in progress',
+      'Increased volume of accepted and published work, including four award-winning publications',
+      'Assisted on a major website redesign, a transition from print to digital media, and indexing',
+      'Provided original ideas, phrasing, and editing of marketing materials resulting in 15% growth of subscriptions',
+      'Provided developmental editing and copyediting for in-house book-publishing efforts',
+      'Continued leadership of two departments in the magazine: The Dog-Eared Page (reprints of classics) and Sunbeams (quotes)'
+    ]
+  },
+  {
+    company: 'The Sun Magazine',
+    position: 'Editorial Associate',
+    link: 'https://www.thesunmagazine.org',
+    daterange: 'September 2009 - December 2011',
+    points: [
+      'Created and led a new department in the magazine: The Dog-Eared Page, featuring reprints of classics',
+      'Fact-checked, copyedited, and proofread issues after design',
+      'Led writing workshops'
+    ]
+  },
+  {
+    company: 'The Sun Magazine',
+    position: 'Editorial Assistant',
+    link: 'https://www.thesunmagazine.org',
+    daterange: 'September 2003 - August 2009',
+    points: [
+      'Read, evaluated, and edited manuscript submissionss for publication'
+    ]
+  },
+  {
+    company: 'Asian University of Science and Technology, Chonburi, Thailand',
+    position: 'Adjunct English Instructor',
+    link: 'http://www.asianu.ac.th/',
+    daterange: 'January 2003 - June 2003',
+    points: [
+      'Developed introductory-level curriculum for English as a Second Language',
+      'Taught summer-school English courses'
+    ]
+  }
+  // {
+  //   company: 'United Nations Environment Programme, Nairobi, Kenya',
+  //   position: 'College Research Intern',
+  //   link: 'https://facebook.com',
+  //   daterange: 'May 1998 - July 1998',
+  //   points: [
+  //     'Performed research on climate change',
+  //     'Developed an informational series published in league with the United Nations International Children Emergency Fund (UNICEF) and distributed in schools throughout Kenya, reagarding environmental health hazards to children',
+  //     'Drafted a speech on Southeast Asian forest fires and respiratory health, delivered by a UN official in Malaysia '
+  //   ]
+  // }
+];
+
+export default positions;
