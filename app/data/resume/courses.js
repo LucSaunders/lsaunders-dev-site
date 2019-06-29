0a3ee72ec985fc525b5cef41d52335f1caf6a2ee
LucSaunders <lucsaunders@gmail.com>
2019-06-29 18:20:03 -0400

    Move static folder and entry. Add Courses and Skills components and placeholder courses data.

---
+++ app/data/resume/courses.js
@@ -0,0 +1,10 @@
+const courses = [
+  // {
+  //   title: 'Class1',
+  //   number: 'use for UNC courses from transcripts',
+  //   link: 'http://unc.edu/class/?????/',
+  //   university: 'UNC-Chapel Hill'
+  // }
+];
+
+export default courses;
