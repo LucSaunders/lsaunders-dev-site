e26063001f9598cc8fd440bad0dc37f898a8dbd6
LucSaunders <lucsaunders@gmail.com>
2019-07-05 22:06:18 -0400

    Add contact data.

---
+++ app/data/contact.js
@@ -0,0 +1,41 @@
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebookF';
+import faSpotify from '@fortawesome/fontawesome-free-brands/faSpotify';
+import faMediumM from '@fortawesome/fontawesome-free-brands/faMediumM';
+import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope';
+
+const data = [
+  {
+    link: 'https://www.linkedin.com/in/lucsaunders',
+    label: 'LinkedIn',
+    icon: faLinkedinIn
+  },
+  {
+    link: 'https://github.com/lucsaunders',
+    label: 'Github',
+    icon: faGithub
+  },
+  {
+    link: 'https://facebook.com/d',
+    label: 'Facebook',
+    icon: faFacebook
+  },
+  {
+    link: 'https://open.spotify.com/user/_vin_fiz?si=SRFhYpzBSUeRwiIeEccEYA',
+    label: 'Spotify',
+    icon: faSpotify
+  },
+  {
+    link: 'https://medium.com/@lucsaunders',
+    label: 'Medium',
+    icon: faMediumM
+  },
+  {
+    link: 'mailto:lucsaunders@gmail.com',
+    label: 'Email',
+    icon: faEnvelope
+  }
+];
+
+export default data;
