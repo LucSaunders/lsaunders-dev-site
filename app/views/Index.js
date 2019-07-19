29b6fb4ce08dac9b2af57b6bd3846c5ff2765944
LucSaunders <lucsaunders@gmail.com>
2019-07-19 16:34:10 -0400

    Modify language in Index / landing page.

--- app/views/Index.js
+++ app/views/Index.js
@@ -8,21 +8,23 @@ const Index = () => (
     <article className='post' id='index'>
       <header>
         <div className='title'>
-          {/* <h2><Link to="/">About this site</Link></h2> */}
-          <p>A responsive React app.</p>
+          <h2>
+            <Link to='/'>About this site</Link>
+          </h2>
+          <p>A responsive React app</p>
         </div>
       </header>
       <p>
         {' '}
-        Welcome to my website. You can read <Link to='/about'>about me</Link>,
-        or check out my <Link to='/resume'>resume</Link>,{' '}
-        <Link to='/projects'>projects</Link>, or view my{' '}
+        Welcome to my website. You can read <Link to='/about'>about me </Link>
+        or view my <Link to='/resume'>resume</Link>, my{' '}
+        <Link to='/projects'>projects</Link>, or my{' '}
         <Link to='/stats'> personal stats</Link>.
         {/* or <Link to="/contact">contact</Link> me. */}
       </p>
       <p>
         {' '}
-        Source available{' '}
+        Site source code available{' '}
         <a href='https://github.com/lucsaunders/lsaunders-dev-site'>here</a>.
       </p>
     </article>
