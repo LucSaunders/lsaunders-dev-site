6798eddc4e0a51eb075ae8e0fd59ef27b2487120
LucSaunders <lucsaunders@gmail.com>
2019-07-19 16:47:03 -0400

    Remove Skills and Courses from Resume page.

--- app/views/Resume.js
+++ app/views/Resume.js
@@ -6,11 +6,11 @@ import Main from '../layouts/Main';
 
 import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
+// import Skills from '../components/Resume/Skills';
+// import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
 
-import courses from '../data/resume/courses';
+// import courses from '../data/resume/courses';
 import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
 import { skills, categories } from '../data/resume/skills';
@@ -18,33 +18,34 @@ import { skills, categories } from '../data/resume/skills';
 const sections = [
   'Education',
   'Experience',
-  'Skills',
-  'Courses',
-  'References',
+  // 'Skills',
+  // 'Courses',
+  'References'
 ];
 
 const Resume = () => (
   <Main>
-    <Helmet title="Resume" />
-    <article className="post" id="resume">
+    <Helmet title='Resume' />
+    <article className='post' id='resume'>
       <header>
-        <div className="title">
-          <h2><Link to="resume">Resume</Link></h2>
-          <div className="link-container">
+        <div className='title'>
+          <h2>
+            <Link to='resume'>Resume</Link>
+          </h2>
+          <div className='link-container'>
             {sections.map(sec => (
               <h4 key={sec}>
                 <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-              </h4>))}
+              </h4>
+            ))}
           </div>
-
         </div>
       </header>
       <Education data={degrees} />
       <Experience data={positions} />
-      <Skills skills={skills} categories={categories} />
-      <Courses data={courses} />
+      {/* <Skills skills={skills} categories={categories} /> */}
+      {/* <Courses data={courses} /> */}
       <References />
-
     </article>
   </Main>
 );
