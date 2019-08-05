9ee2c3b3cf1d82a1aecc405aff092eb872581cc3
LucSaunders <lucsaunders@gmail.com>
2019-08-05 08:53:17 -0400

    Remove link from References.

--- app/components/Resume/References.js
+++ app/components/Resume/References.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const References = () => (
-  <div className="references">
-    <div className="link-to" id="references" />
-    <div className="title">
-      <Link to="/contact">
-        <h3>References are available upon request</h3>
-      </Link>
+  <div className='references'>
+    <div className='link-to' id='references' />
+    <div className='title'>
+      <h3>References are available upon request</h3>
     </div>
   </div>
 );
