fbec9098e53e99f7f7b4c1afe56c161c9368ba7d
LucSaunders <lucsaunders@gmail.com>
2019-08-05 14:23:07 -0400

    Add skills and styling to Resume. Disable skillbars and clickhandlers for skills.

---
+++ app/data/resume/skills.js
@@ -0,0 +1,86 @@
+// TODO: Add Soft Skills, Office Skills, Editing/Writing Skills, Athletic Skills,  (?)
+export const categories = [
+  {
+    name: 'HTML'
+  },
+  {
+    name: 'CSS'
+  },
+  {
+    name: 'CSS Grid'
+  },
+  { name: 'CSS Flexbox' },
+  {
+    name: 'SASS/SCSS'
+  },
+  {
+    name: 'Bootstrap'
+  },
+  {
+    name: 'jQuery'
+  },
+  {
+    name: 'React'
+  },
+  { name: 'Redux' },
+  {
+    name: 'Backbone'
+  },
+  {
+    name: 'GraphQL'
+  },
+  {
+    name: 'Apollo'
+  },
+  { name: 'REST' },
+  {
+    name: 'Node'
+  },
+  {
+    name: 'Express'
+  },
+
+  {
+    name: 'Mongo'
+  },
+  {
+    name: 'MySQL',
+
+    category: ['Databases']
+  },
+  {
+    name: 'PostgreSQL',
+
+    category: ['Databases']
+  },
+  {
+    name: 'Firebase',
+
+    category: ['Databases']
+  },
+  {
+    name: 'Heroku'
+  },
+  {
+    name: 'Git',
+
+    category: ['Tools']
+  },
+  {
+    name: 'Wireframing',
+
+    category: ['Design']
+  },
+  {
+    name: 'Chai'
+  },
+  {
+    name: 'Mocha'
+  },
+
+  {
+    name: 'Swagger'
+  }
+];
+
+export const skills = [];
--- app/views/Resume.js
+++ app/views/Resume.js
@@ -6,13 +6,16 @@ import Main from '../layouts/Main';
 
 import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
+import Skills from '../components/Resume/Skills';
 import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
+
 import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
+import { skills, categories } from '../data/resume/skills';
 import courses from '../data/resume/courses';
 
-const sections = ['Education', 'Experience', 'Courses', 'References'];
+const sections = ['Education', 'Experience', 'Skills', 'Courses', 'References'];
 
 const Resume = () => (
   <Main>
@@ -34,6 +37,7 @@ const Resume = () => (
       </header>
       <Education data={degrees} />
       <Experience data={positions} />
+      <Skills skills={skills} categories={categories} />
       <Courses data={courses} />
       <References />
     </article>
