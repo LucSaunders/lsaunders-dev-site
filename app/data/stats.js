2498de3770def6769059ef8c05c38734b5ccc541
LucSaunders <lucsaunders@gmail.com>
2019-08-10 22:39:09 -0400

    Add links to cities in stats data. Change scrollover accent to match border-alt in scss vars partial.

--- app/data/stats.js
+++ app/data/stats.js
@@ -1,12 +1,15 @@
 const data = {
   birthplace: {
     label: 'Birthplace',
-    value:
-      "Rocky Mount, NC"
+    key: 'birthplace',
+    value: 'Rocky Mount, NC',
+    link: 'https://en.wikipedia.org/wiki/Rocky_Mount,_North_Carolina'
   },
   location: {
-    label: 'Cities I currently call "home"',
-    value: 'Durham, NC, and Charleston, SC'
+    label: 'City I currently call "home"',
+    key: 'home1',
+    value: 'Durham, NC',
+    link: 'https://durhamnc.gov/'
   },
   // countries: {
   //   label: 'Countries visited',
