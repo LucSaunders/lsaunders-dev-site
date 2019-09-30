050f700d0778b799073e73e37f7bc08e501957c8
Luc Saunders <lucsaunders@gmail.com>
2019-09-30 12:08:22 -0400

    Restore image paths for Projects.

--- app/data/projects.js
+++ app/data/projects.js
@@ -13,7 +13,7 @@ const data = [
     title: 'Lyrica',
     subtitle: 'Search for song lyrics',
     link: 'https://lucsaunders.github.io/song-lyric-search/#/',
-    image: 'lsaunders-dev-site/images/projects/lyrica.jpg',
+    image: 'images/projects/lyrica.jpg',
     date: '2019-06-27',
     desc: 'Search for lyrics to almost any song. React | Musicmatch API'
   },
@@ -21,7 +21,7 @@ const data = [
     title: 'SpaceX Launch Missions',
     subtitle: 'Tracking successful and failed SpaceX rocket launches.',
     link: 'https://spacex-missions-gql.herokuapp.com/',
-    image: 'lsaunders-dev-site/images/projects/spacex.jpg',
+    image: 'images/projects/spacex.jpg',
     date: '2019-06-25',
     desc:
       'Track the history and statistics of SpaceX rocket launches. React | GraphQL | SpaceX API.'
@@ -30,7 +30,7 @@ const data = [
     title: 'Contact Organizer',
     subtitle: '',
     link: 'https://lucsaunders.github.io/contact-organizer',
-    image: 'lsaunders-dev-site/images/projects/contactorganizer.jpg',
+    image: 'images/projects/contactorganizer.jpg',
     date: '2019-03-25',
     desc:
       'Manage personal contacts. (Full CRUD. For demo purposes only.) React | Context API'
@@ -39,7 +39,7 @@ const data = [
     title: 'Axon',
     subtitle: 'A Hubspot clone ',
     link: 'https://project-hubspot.herokuapp.com/dashboard',
-    image: 'lsaunders-dev-site/images/projects/axon.jpg',
+    image: 'images/projects/axon.jpg',
     date: '2019-05-10',
     desc:
       'A Customer Relationship Management tool for tracking company revenue, clients, and deals. Developed by an eleven-member team of Project Shift software-engineering fellows. Sass | React | Redux | Node | Express | MySQL'
